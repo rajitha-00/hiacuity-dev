26b9e7836530e418c57df7a7b2c8e18d267245bc
rajitha-00 <rajithatw07102@gmail.com>
2024-11-03 23:09:03 +0530

    fix:issue-fixed

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -21,20 +21,20 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link href={props.href ?? "#"} passHref>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
+        <Link
+          href={props.href ?? "#"}
+          passHref
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
         </Link>
       </NavigationMenuLink>
     </li>
@@ -113,7 +113,7 @@ export const Header = () => {
                           >
                             {/* <Icons.logo className="h-6 w-6" /> */}
                             <div className="mb-2 mt-4 text-lg font-medium">
-                              Awolan
+                              HiAcuity
                             </div>
                             <p className="text-sm leading-tight text-muted-foreground">
                               Beautifully designed components that you can copy
